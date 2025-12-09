6ee1f8d9c49c34f3d034204af36cc06192a903d6
Adhal Andres Cabrera Acosta <andres_acosta13@hotmail.com>
2025-12-09 23:34:26 +0000

    Build to docs for GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/VideoPromptHelp/"
+  base: "/VideoPromptHelp/",
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 });
